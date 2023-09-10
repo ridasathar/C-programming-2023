aa83ccf3736dd4d3c62d8fe1feec0cd3f04a91f4
ridasathar <143441622+ridasathar@users.noreply.github.com>
2023-09-11 00:49:42 +0530

    Add files via upload

---
+++ Square root.cpp
@@ -0,0 +1,11 @@
+/* This program is used to find square root of a number */
+# include <math.h>
+# include <stdio.h>
+int main()
+{
+	int a,b;
+	scanf("%d",&a);
+	b=sqrt(a);
+	printf("The square root of a is %d",b);
+	return 0;	
+}
